fb8e079972ea15b12b3d6d1f0c68e803b7c28fb8
faaizj <faaizjamali@gmail.com>
2012-08-01 13:17:55 +0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloudtek::Application.config.secret_token = '730d423a056f8b7ea1aaeef5024597a4ef9fd963238b6d020c0f5bb98401267469d41d8713ff40ab7fe5ed100091aa1dd6e8eed50241a4c8a16b1ae1b65606f2'
